891f2c2f6fc9a44b836eefe37f60907235b94fbb
AppDevIn <teamprojectlive@gmail.com>
2020-11-16 15:20:32 +0800

    Added server code and add default styles and componets

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import App from "next/app";
+import Layout from '../components/_App/Layout';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+
+  static async getInitialProps({Component, ctx}) {
+
+    let pageProps = {}
+
+    if(Component.getInitialProps){
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
+
