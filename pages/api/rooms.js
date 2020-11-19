bbcc7ae050b66ac4b565af8d9cc49f30e8ecf5d5
AppDevIn <teamprojectlive@gmail.com>
2020-11-19 14:04:24 +0800

    Refactor rooms api code

--- pages/api/rooms.js
+++ pages/api/rooms.js
@@ -5,32 +5,9 @@ import Video from '../../models/Video.js'
 connectDb()
 export default async(req, res) => {
 
-    switch (req.method) {
-        case "GET":
-            const type = req.query.type;
-            if (type == '1') {
-                await getAllRooms(req, res);
-            } else {
-                await handleGetRequest(req, res);
-            }
-            break;
-            // case "POST":
-            //     break;
-        default:
-            res.status(405).send(`Method ${req.method} not allowed`)
-            break;
-    }
-}
-
-async function handleGetRequest(req, res) {
     const { _id } = req.query
-    console.log("id", _id);
-    const rooms = await Room.find({ memebers: _id })
-    res.status(200).send(rooms)
-}
 
-async function getAllRooms(req, res) {
-    const rooms = await Room.find()
+    const rooms = await Room.find({ memebers: _id })
 
     for (var i = 0; i < rooms.length; i++) {
         if (rooms[i].Playing != null) {
