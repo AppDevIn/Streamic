40098b8ffd1c7332af7bc3417a3dad7f89576983
hwen <whman63@gmail.com>
2020-11-19 17:28:34 +0800

    fix socket

--- pages/room.js
+++ pages/room.js
@@ -5,6 +5,7 @@ import { Container } from "semantic-ui-react";
 import baseUrl from '../utils/baseUrl';
 import axios from 'axios';
 import ChatBox from '../components/Chat/ChatBox';
+import io from 'socket.io-client';
 
 // Create context container in a global scope so it can be visible by every component
 const ContextContainer = React.createContext(null);
@@ -12,12 +13,16 @@ const ContextContainer = React.createContext(null);
 function Room(props) {
 
     const [parent_link, updateLink] = useState("");
+    const [socket, setSocket] = useState(null);
 
     useEffect(() => {
         document.body.style.backgroundColor = "#242A2E";
+        if (socket == null){
+            setSocket(io());
+        }
     }, [])
 
-    return <ContextContainer.Provider value={{ parent_link, updateLink }}>
+    return <ContextContainer.Provider value={{ parent_link, updateLink, socket, setSocket }}>
         <PlayerHeader />
         <Container fluid className="mt-5 ct">
             <YoutubePlayer  {...props} />
