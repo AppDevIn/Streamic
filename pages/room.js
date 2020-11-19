7138c9f91b6ea5b1b72d2e61b2fe245e1fdb4fbf
ZazzyDictionary <zac.hong01@gmail.com>
2020-11-19 14:44:49 +0800

    change chatroom css

--- pages/room.js
+++ pages/room.js
@@ -17,21 +17,21 @@ function Room(props) {
         document.body.style.backgroundColor = "#242A2E";
     }, [])
 
-    return <ContextContainer.Provider value={{ parent_link, updateLink}}>
+    return <ContextContainer.Provider value={{ parent_link, updateLink }}>
         <PlayerHeader />
         <Container fluid className="mt-5 ct">
-            <YoutubePlayer  {...props}/>
-            <ChatBox/>
+            <YoutubePlayer  {...props} />
+            <ChatBox {...props} />
         </Container>
     </ContextContainer.Provider>
 }
 
 Room.getInitialProps = async (ctx, user) => {
     const url = `${baseUrl}/api/room`
-    const payload = { params: { roomID : ctx.query._id , _id: user._id } };
+    const payload = { params: { roomID: ctx.query._id, _id: user._id } };
     const response = await axios.get(url, payload);
-    return {roomInfo : response.data};
+    return { roomInfo: response.data };
 }
 
-export {ContextContainer};
+export { ContextContainer };
 export default Room;
