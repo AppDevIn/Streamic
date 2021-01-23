35698836cc9f1ffc8001a4a0c35e17d0010178f9
hwen <whman63@gmail.com>
2021-01-23 20:58:08 +0800

    init link

--- components/player/player.js
+++ components/player/player.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState, useRef, useContext} from 'react';
 import ReactPlayer from 'react-player/lazy'
 
 function Player() {
-    const [url, setUrl] = useState("https://www.twitch.tv/videos/875820997")
+    const [url, setUrl] = useState("https://www.youtube.com/watch?v=jXQg_1NvF4U")
     const [playing, setPlaying] = useState(true)
     const [isFullScreen, setFullScreen] = useState(false)
 
     
     return <ReactPlayer
-        url="https://www.twitch.tv/videos/875820997"
-        config={{
+        url = {url}
+        config = {{
             youtube: {
             playerVars: { showinfo: 0,
             controls: 0, disablekb: 0, modestbranding: 0, rel: 0 }}
