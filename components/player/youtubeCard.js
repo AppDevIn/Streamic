58c18691b68b14cf0059a536ddeca003be56e2c2
ZazzyDictionary <zac.hong01@gmail.com>
2021-01-24 00:39:36 +0800

    :sparkles: updated video card

--- components/player/youtubeCard.js
+++ components/player/youtubeCard.js
@@ -1,32 +1,58 @@
-import React from 'react'
-import { Card, Image } from 'semantic-ui-react'
+import { ContactsOutlined } from '@material-ui/icons'
+import React, { useState } from 'react'
+import { Card, Image, Button, Dimmer, Segment, Icon, Divider } from 'semantic-ui-react'
 
 
 function addVideoToPlaylist(video) {
 
-
-
 }
 
-const youtubeCard = (props) => (
-  <Card onClick={props.onClick}>
-    <div class="blurring dimmable image">
-      <div class="ui dimmer">
-        <div class="content">
-          <div class="center">
-            <div class="ui inverted button">Add Friend</div>
-          </div>
-        </div>
-      </div>
-      <Image src={props.info.thumbnail} wrapped ui={false} />
-    </div>
+// function whichSocialPlatfrom(urlLink) {
+//   if (urlLink.contains("https://www.twitch.tv/")) {
+//     return { "social": "Twitch", "socialName": "twitch" }
+//   } else if (urlLink.contains("https://www.youtube.com/watch?v=")) {
+//     return { "social": "Youtube", "socialName": "youtube" }
+//   } else if (urlLink.contains("https://fb.watch/")) {
+//     return { "social": "Facebook", "socialName": "facebook" }
+//   }
+// }
+
+const socialCard = (props) => (
+  <Button color={props.socialName}>
+    <Icon name={props.socialName} /> {props.social}
+  </Button>
+)
+
+function youtubeCard(props) {
+  const [active, setActive] = useState(false)
+  // const [social, socialName] = whichSocialPlatfrom(props.info)
+  return < Card onClick={props.onClick} onMouseOver={(e) => {
+    setActive(true)
+  }} onMouseLeave={(e) => {
+    setActive(false)
+  }}>
+    <Dimmer.Dimmable as={Segment} dimmed={active}>
+      <Image src={props.info.thumbnail} wrapped ui={false} fluid />
+
+      <Dimmer active={active}>
+        <Button.Group vertical floated='right'>
+          <Button color="youtube" enabled="false" icon='youtube' />
+          <Divider />
+          <Button icon='add' color='red' />
+
+        </Button.Group>
+      </Dimmer>
+    </Dimmer.Dimmable>
+
     <Card.Content>
       <Card.Header>{props.info.title}</Card.Header>
       <Card.Meta>
         <span>{props.info.publisher}</span>
+
       </Card.Meta>
     </Card.Content>
-  </Card>
-)
+  </ Card>
+
+}
 
 export default youtubeCard
