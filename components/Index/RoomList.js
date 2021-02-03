6dc842a7e899110034887bb3c8f0ec5cd1812d41
ZazzyDictionary <zac.hong01@gmail.com>
2021-02-03 13:51:04 +0800

    :sparkles: added copy room code

--- components/Index/RoomList.js
+++ components/Index/RoomList.js
@@ -1,27 +1,37 @@
-import { Card, Image  } from 'semantic-ui-react'
+import { Card, Image } from 'semantic-ui-react'
 import Layout from '../Index/Layout';
 
-function RoomList({ rooms }) {
+function RoomList({rooms}) {
 
     function mapProductsToItems(rooms) {
         return rooms.map(room => (
-            <Card href={`/room?_id=${room.roomID}`} raised key={room._id} >
-             <Image size="large" circular src={room.mediaUrl} style={{ maxHeight: 200 }}/>
-             <Card.Content>
-                 <Card.Header>{room.roomName}</Card.Header>
-             </Card.Content>
-         </Card>
+            <Card raised key={ room._id }>
+              <Image href={ `/room?_id=${room.roomID}` } size="large" circular src={ room.mediaUrl } style={ { maxHeight: 200 } } />
+              <Card.Content>
+                <Card.Header>
+                  { room.roomName }
+                </Card.Header>
+              </Card.Content>
+              <Card.Content extra>
+                <Button basic color='grey' onClick={ () => {
+                                                         navigator.clipboard.writeText(room.roomID)
+                                                     } }>
+                  Copy Room Code
+                </Button>
+              </Card.Content>
+            </Card>
         ));
     }
 
     return (
-    // <Layout>
-    <Card.Group doubling stackable itemsPerRow = "6"
-    >{mapProductsToItems(rooms)}</Card.Group>
-    // </Layout>
-    );
-    
-    
+        // <Layout>
+        <Card.Group doubling stackable itemsPerRow="6">
+          { mapProductsToItems(rooms) }
+        </Card.Group>
+        // </Layout>
+        );
+
+
 }
 
 export default RoomList;
