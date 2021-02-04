4f07dec3fe0965a2d8b1f5c299328671442c71e4
AppDevIn <teamprojectlive@gmail.com>
2021-02-04 11:35:03 +0800

    🎨 Formatted code

--- pages/room.js
+++ pages/room.js
@@ -1,80 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import PlayerHeader from "../components/_App/PlayerHeader"
+import React, { useEffect, useState } from "react";
+import PlayerHeader from "../components/_App/PlayerHeader";
 import { Container } from "semantic-ui-react";
-import baseUrl from '../utils/baseUrl';
-import axios from 'axios';
-import ChatBox from '../components/Chat/Chatbox';
-import VoiceChat from '../components/Voice/voiceChat';
-import io from 'socket.io-client';
-import Router from 'next/router'
-
+import baseUrl from "../utils/baseUrl";
+import axios from "axios";
+import ChatBox from "../components/Chat/Chatbox";
+import VoiceChat from "../components/Voice/voiceChat";
+import io from "socket.io-client";
+import Router from "next/router";
 
 // Create context container in a global scope so it can be visible by every component
 const ContextContainer = React.createContext(null);
 
 function Room(props) {
+  const [parent_link, updateLink] = useState("");
+  const [socket, setSocket] = useState(null);
 
-    const [parent_link, updateLink] = useState("");
-    const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    document.body.style.backgroundColor = "#242A2E";
+    if (socket == null) {
+      setSocket(io());
+    }
+  }, []);
 
-    useEffect(() => {
-        document.body.style.backgroundColor = "#242A2E";
-        if (socket == null) {
-            setSocket(io());
+  useEffect(() => {
+    if (socket != null) {
+      socket.emit("usersToRoom", props.user._id);
 
-
-
+      socket.on("retrieve usersToRoom", (room) => {
+        if (room) {
+          Router.push("/index");
         }
-    }, [])
+      });
 
+      socket.emit("joinRoom", { roomID: props.roomID, user: props.user });
+    }
+  }, [socket]);
 
-    
-
-
-
-    useEffect(() => {
-        
-        if (socket != null) {
-
-            socket.emit("usersToRoom", props.user._id);
-
-            socket.on("retrieve usersToRoom", (room) => {
-                if (room) {
-                    Router.push('/index')
-                }
-            })
-        
-            socket.emit("joinRoom", ({roomID:props.roomID, user:props.user}));
-
-            
-
-    
-
-            
-
-
-        }
-    }, [socket])
-
-    return <ContextContainer.Provider value={{ parent_link, updateLink, socket, setSocket }}>
-        <PlayerHeader />
-        <Container fluid className="mt-5 ct">
-            <ChatBox {...props} />
-        </Container>
-    </ContextContainer.Provider >
+  return (
+    <ContextContainer.Provider
+      value={{ parent_link, updateLink, socket, setSocket }}
+    >
+      <PlayerHeader />
+      <Container fluid className="mt-5 ct">
+        <ChatBox {...props} />
+      </Container>
+    </ContextContainer.Provider>
+  );
 }
 
 Room.getInitialProps = async (ctx, user) => {
-    var url = `${baseUrl}/api/room`
-    const payload = { params: { roomID: ctx.query._id, _id: user._id } };
-    const responseRoom = await axios.get(url, payload);
+  var url = `${baseUrl}/api/room`;
+  const payload = { params: { roomID: ctx.query._id, _id: user._id } };
+  const responseRoom = await axios.get(url, payload);
 
+  url = `${baseUrl}/api/messages`;
+  const responseMessage = await axios.get(url, {
+    params: { roomID: ctx.query._id },
+  });
 
-    url = `${baseUrl}/api/messages`
-    const responseMessage = await axios.get(url, { params: { roomID: ctx.query._id } });
-
-    return { roomInfo: responseRoom.data, user:user , roomID: ctx.query._id, messages: responseMessage.data };
-}
+  return {
+    roomInfo: responseRoom.data,
+    user: user,
+    roomID: ctx.query._id,
+    messages: responseMessage.data,
+  };
+};
 
 export { ContextContainer };
 export default Room;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const next = require('next');
-const baseUrl = process.env.NODE_ENV === "production" ?
-    'https://p2streamic.herokuapp.com' :
-    'http://localhost:3000';
-const axios = require('axios').default;
-require('dotenv').config();
-
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const next = require("next");
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://p2streamic.herokuapp.com"
+    : "http://localhost:3000";
+const axios = require("axios").default;
+require("dotenv").config();
 
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
@@ -20,246 +20,201 @@ require('dotenv').config();
 
 // import register from './pages/api/register'
 
-
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
-const nextHandler = nextApp.getRequestHandler()
-
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
 
 //Keep track of the users in each room
-const users = {}
-const inRoom = {}
+const users = {};
+const inRoom = {};
 
-const muted = {}
+const muted = {};
 
 const socketToRoom = {};
 const userToRoom = {};
 const socketToUser = {};
 
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ roomID, user }) => {
+    console.log(user);
+    console.log(`${user._id} has joined the ${roomID}`);
+    socket.emit("message", "Welcome to Streamic.");
+    socket.join(roomID);
 
-io.on('connection', socket => {
-    socket.on('joinRoom', ({ roomID, user }) => {
-        console.log(user);
-        console.log(`${user._id} has joined the ${roomID}`);
-        socket.emit("message", "Welcome to Streamic.");
-        socket.join(roomID);
+    userToRoom[user._id] = true;
+    socketToUser[socket.id] = user._id;
+  });
 
-        userToRoom[user._id] = true
-        socketToUser[socket.id] = user._id
+  socket.on("usersToRoom", (user) => {
+    console.log("id of user " + user);
+    let room = userToRoom[user];
 
+    console.log("retrieve the room " + room);
 
+    io.to(socket.id).emit("retrieve usersToRoom", userToRoom[user]);
+  });
 
+  socket.on("changes", ({ roomID, data }) => {
+    io.to(roomID).emit("streaming", data);
+    if (data.isVideoChanged) {
+      // update the video playing for the room
+      async function updateRoomWatching() {
+        const url = `${baseUrl}/api/room?type=1`;
+        const payload = { roomID, data };
+        const response = await axios.post(url, payload);
+      }
 
+      updateRoomWatching();
+    }
+  });
+
+  socket.on("router", (roomID) => {
+    socket.broadcast.to(roomID).emit("existingUser");
+  });
+
+  socket.on("sendMessage", (data) => {
+    const { message, roomID } = data;
+    console.log("Message reveived from " + roomID);
+    io.to(roomID).emit("messageChanges", message);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
     });
+  });
 
-
-    socket.on("usersToRoom", (user) => {
-
-        console.log("id of user " + user);
-        let room = userToRoom[user]
-
-        console.log("retrieve the room " + room);
-
-        io.to(socket.id).emit("retrieve usersToRoom", userToRoom[user])
-    })
-
-    socket.on('changes', ({ roomID, data }) => {
-        io.to(roomID).emit('streaming', data);
-        if (data.isVideoChanged) {
-            // update the video playing for the room
-            async function updateRoomWatching() {
-                const url = `${baseUrl}/api/room?type=1`
-                const payload = { roomID, data }
-                const response = await axios.post(url, payload)
-            }
-
-            updateRoomWatching();
-        }
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
     });
-
-    socket.on('router', (roomID) => {
-        socket.broadcast.to(roomID).emit('existingUser');
-    });
-
-    socket.on('sendMessage', (data) => {
-        const { message, roomID } = data
-        console.log("Message reveived from " + roomID);
-        io.to(roomID).emit('messageChanges', message);
-    })
-
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
-
-
-    socket.on("get member", (roomID) => {
-        io.to(roomID).emit("memeber join", inRoom[roomID])
-    });
-
-    socket.on("memeber add", ({ roomID, user }) => {
-
-        if (inRoom[roomID]) {
-
-            try {
-                //Add the room
-                inRoom[roomID].push({...user, sid: socket.id });
-                console.log(`User ${user.username}`);
-
-            } catch (error) {
-                console.log("Error getting the user", error);
-            }
-
-
-        } else {
-
-            try {
-                //Create a new room object
-                inRoom[roomID] = [user];
-
-
-            } catch (error) {
-                console.log("Error getting the user", error);
-            }
-
-        }
-
-
-
-
-        console.log(`Member in the room: ${inRoom[roomID]}`);
-
-
-
-    });
-
-
-
-    socket.on("mute user", ({ roomID }) => {
-
-        //Check if the room exist
-        if (muted[roomID]) {
-
-            //Add the room
-            muted[roomID].push(socket.id);
-        } else {
-            //Create a new room object
-            muted[roomID] = [socket.id];
-        }
-
-        io.to(roomID).emit("muted user", muted[roomID])
-
-    });
-
-
-    socket.on("unmute user", ({ roomID }) => {
-        console.log("Unmuting the " + socket.id);
-
-        console.log(muted[roomID]);
-        muted[roomID] = (muted[roomID]).filter(m => m !== socket.id)
-        io.to(roomID).emit("muted user", muted[roomID])
-
-    });
-
-
-    // socket.on("get mute user", ({ roomID }) => {
-    //     io.to(roomID).emit("muted user", muted[roomID])
-    // });
-
-
-
-    socket.on("join room", ({ roomID, user }) => {
-        //Check if the room exist
-        if (users[roomID]) {
-
-            //Add the room
-            users[roomID].push(socket.id);
-        } else {
-            //Create a new room object
-            users[roomID] = [socket.id];
-        }
-
-        socketToRoom[socket.id] = roomID;
-
-        //Get the users in the room
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-        console.log(`Users in the room: ${usersInThisRoom}`);
-
-
-        //Send all the exisitng to user details to clients
-        console.log("Users");
-        console.log(usersInThisRoom);
-        socket.emit("all users", usersInThisRoom);
-
-
-
-
-
-    });
-
-
-
-    // Runs when client disconnects
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} has left the room`);
-
-        //For voice 
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-        }
-
-        let rooms = inRoom[roomID];
-        if (rooms) {
-            rooms = rooms.filter(user => user.sid !== socket.id);
-            inRoom[roomID] = rooms;
-        }
-
-        let mute = muted[roomID];
-        if (mute) {
-            mute = mute.filter(m => m !== socket.id);
-            inRoom[roomID] = mute;
-        }
-
-        let uid = socketToUser[socket.id]
-        if (uid) {
-            if (userToRoom[uid]) {
-                userToRoom[uid] = false
-            }
-        }
-
-        io.to(roomID).emit("remove socket", socket.id)
-
-
-
-
-    });
-
+  });
+
+  socket.on("get member", (roomID) => {
+    io.to(roomID).emit("memeber join", inRoom[roomID]);
+  });
+
+  socket.on("memeber add", ({ roomID, user }) => {
+    if (inRoom[roomID]) {
+      try {
+        //Add the room
+        inRoom[roomID].push({ ...user, sid: socket.id });
+        console.log(`User ${user.username}`);
+      } catch (error) {
+        console.log("Error getting the user", error);
+      }
+    } else {
+      try {
+        //Create a new room object
+        inRoom[roomID] = [user];
+      } catch (error) {
+        console.log("Error getting the user", error);
+      }
+    }
+
+    console.log(`Member in the room: ${inRoom[roomID]}`);
+  });
+
+  socket.on("mute user", ({ roomID }) => {
+    //Check if the room exist
+    if (muted[roomID]) {
+      //Add the room
+      muted[roomID].push(socket.id);
+    } else {
+      //Create a new room object
+      muted[roomID] = [socket.id];
+    }
+
+    io.to(roomID).emit("muted user", muted[roomID]);
+  });
+
+  socket.on("unmute user", ({ roomID }) => {
+    console.log("Unmuting the " + socket.id);
+
+    console.log(muted[roomID]);
+    muted[roomID] = muted[roomID].filter((m) => m !== socket.id);
+    io.to(roomID).emit("muted user", muted[roomID]);
+  });
+
+  // socket.on("get mute user", ({ roomID }) => {
+  //     io.to(roomID).emit("muted user", muted[roomID])
+  // });
+
+  socket.on("join room", ({ roomID, user }) => {
+    //Check if the room exist
+    if (users[roomID]) {
+      //Add the room
+      users[roomID].push(socket.id);
+    } else {
+      //Create a new room object
+      users[roomID] = [socket.id];
+    }
+
+    socketToRoom[socket.id] = roomID;
+
+    //Get the users in the room
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+    console.log(`Users in the room: ${usersInThisRoom}`);
+
+    //Send all the exisitng to user details to clients
+    console.log("Users");
+    console.log(usersInThisRoom);
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} has left the room`);
+
+    //For voice
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+
+    let rooms = inRoom[roomID];
+    if (rooms) {
+      rooms = rooms.filter((user) => user.sid !== socket.id);
+      inRoom[roomID] = rooms;
+    }
+
+    let mute = muted[roomID];
+    if (mute) {
+      mute = mute.filter((m) => m !== socket.id);
+      inRoom[roomID] = mute;
+    }
+
+    let uid = socketToUser[socket.id];
+    if (uid) {
+      if (userToRoom[uid]) {
+        userToRoom[uid] = false;
+      }
+    }
+
+    io.to(roomID).emit("remove socket", socket.id);
+  });
 });
 
 nextApp.prepare().then(() => {
-    app.get('*', (req, res) => {
-        return nextHandler(req, res)
-    })
+  app.get("*", (req, res) => {
+    return nextHandler(req, res);
+  });
 
-    app.post('*', async(req, res) => {
-            return await nextHandler(req, res)
-        })
-        // app.post("/api/register", (req, res) => {
-        //     register(req, res)
-        // })
+  app.post("*", async (req, res) => {
+    return await nextHandler(req, res);
+  });
+  // app.post("/api/register", (req, res) => {
+  //     register(req, res)
+  // })
 
-
-    server.listen(port, (err) => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-    })
-})
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
